20a74c0712cb006b820d0471ce078cc0738ef359
Yazan Al-Sedih <137224224+yazansedih@users.noreply.github.com>
2025-05-21 07:50:34 +0300

    Time: 985 ms (57.68%), Space: 0B (100%) - LeetHub

--- 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
@@ -2,7 +2,7 @@
 SELECT 
     r.contest_id,
     ROUND(
-        COUNT( r.user_id) * 100.0 / (SELECT COUNT(*) FROM Users),
+        COUNT(DISTINCT r.user_id) * 100.0 / (SELECT COUNT(*) FROM Users),
         2
     ) AS percentage
 FROM Register r
