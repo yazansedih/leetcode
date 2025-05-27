1c76bbb617b1638312e7a2487cffaf40877070a0
Yazan Al-Sedih <137224224+yazansedih@users.noreply.github.com>
2025-05-27 06:46:11 +0300

    Time: 244 ms (5.03%), Space: 64.1 MB (5.74%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cs
+++ 0011-container-with-most-water/0011-container-with-most-water.cs
@@ -3,9 +3,11 @@ public class Solution {
         int l = 0, r = height.Length - 1;
         int area = 0;
 
-        while(l < r) {
-            area = Math.Max(area, Math.Min(height[l], height[r]) * (r - l));
-            if(height[l] >= height[r]) r--;
+        while (l < r) {
+            int currentArea = Math.Min(height[l], height[r]) * (r - l); 
+            area = Math.Max(area, currentArea);
+
+            if (height[l] >= height[r]) r--;
             else l++;
         }
 
