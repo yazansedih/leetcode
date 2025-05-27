2c441f0a890d6c24f0770bb3a0bf88a28e86c3f2
Yazan Al-Sedih <137224224+yazansedih@users.noreply.github.com>
2025-05-27 06:34:56 +0300

    Time: 1 ms (91.59%), Space: 29.3 MB (74.77%) - LeetHub

--- 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cs
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cs
@@ -2,11 +2,10 @@ public class Solution {
     public int DifferenceOfSums(int n, int m) {
         int sum1 = 0;
         int sum2 = 0;
-        for(int i = 1; i <= n; i++) {
-            if(i % m == 0) sum1 += i;
-            else sum2 += i;
+        for (int i = 1; i <= n; i++) {
+            if (i % m == 0) sum1 += i;  // divisible by m
+            else sum2 += i;             // not divisible by m
         }
-
         return sum2 - sum1;
     }
 }
