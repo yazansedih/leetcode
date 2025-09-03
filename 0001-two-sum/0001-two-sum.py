22c102192c751d3609f38e376e055703dabc2e09
Yazan Al-Sedih <137224224+yazansedih@users.noreply.github.com>
2025-09-03 15:33:34 +0300

    Time: 0 ms (100%), Space: 13.1 MB (77.41%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_map = {}  # value -> index
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
+
+        # Problem guarantees one solution, but just in case
+        return []
